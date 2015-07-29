5d20d9cfe4c8da1162c3e73393c3f5f6a038139c
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-07-29 11:54:20 -0400

    adds a user agent

--- lib/flowthings/connection.rb
+++ lib/flowthings/connection.rb
@@ -4,20 +4,22 @@ require 'flowthings/error'
 
 module Flowthings
   module Connection
-
     private
+
     def connection
       url = @endpoint
       if @secure
-        url = "https://" + url
+        url = 'https://' + url
       else
-        url = "http://" + url
+        url = 'http://' + url
       end
 
       @connection = Excon.new(url,
-                              headers: {"X-Auth-Token" => @account_token,
-                                        "Content-Type" => "application/json"})
+                              headers: {
+                                'X-Auth-Token' => @account_token,
+                                'Content-Type' => 'application/json',
+                                'User-Agent' => @user_agent
+                              })
     end
-
   end
 end
